0db7989487b712326fe18d9f03bd5b9eff9139b3
Micael <98262812+MicaelSv@users.noreply.github.com>
2024-03-14 21:56:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -75,9 +75,10 @@ def getpacientes():
 @app.route('/filas', methods=['POST'])
 def verificar_cpf():
 
-    cpf = request.get_json()  #recebendo o cpf do front
-    #cpf = data['cpf']
+    data = request.get_json()  #recebendo o cpf do front
+    cpf = data['cpf']
 
+    print(cpf)
 
     # Verifica se o CPF já está na fila
     position_in_queue = 0
